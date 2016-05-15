c08b1016b30b0aa05e7c638ba30817d3a3728bce
John Rocha <john.rocha5@gmail.com>
2016-05-15 04:50:37 -0500

    Intial project creation

---
+++ DynamicBG-OSX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DynamicBG-OSX
+//
+//  Created by John Rocha on 5/15/16.
+//  Copyright © 2016 John Rocha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
