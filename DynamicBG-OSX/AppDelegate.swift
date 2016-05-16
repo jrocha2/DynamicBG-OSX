60efdd978184e4248f200f494f4e425766662a65
John Rocha <john.rocha5@gmail.com>
2016-05-16 12:56:51 -0500

    Restores dynamic background upon waking up if it was enabled when the user put the computer to sleep

--- DynamicBG-OSX/AppDelegate.swift
+++ DynamicBG-OSX/AppDelegate.swift
@@ -24,12 +24,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         statusItem.image = icon
         statusItem.menu = statusMenu
+        
+        NSWorkspace.sharedWorkspace().notificationCenter.addObserver(self, selector: #selector(applicationWillWake), name: NSWorkspaceDidWakeNotification, object: nil)
     }
-
+    
+    func applicationWillWake(aNotification: NSNotification) {
+        if statusMenu.itemArray[0].state == NSOnState {
+            setDynamicBGEnabled(true)
+        }
+    }
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         setDynamicBGEnabled(false)
     }
-
+    
     @IBAction func menuClicked(sender: NSMenuItem) {
         if sender.state == NSOnState {
             sender.state = NSOffState
